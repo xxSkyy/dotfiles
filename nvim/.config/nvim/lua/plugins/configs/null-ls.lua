e0a223b5c3168b94749a00bcbeec8c30e7eb7351
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2023-11-22 03:01:48 +0100

    feat: delete rustfmt from null-ls

--- nvim/.config/nvim/lua/plugins/configs/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/configs/null-ls.lua
@@ -4,7 +4,7 @@ null_ls.setup({
   on_attach = require("lsp-format").on_attach,
   sources = {
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.rustfmt,
+    -- null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.completion.luasnip,
     null_ls.builtins.formatting.astyle,
