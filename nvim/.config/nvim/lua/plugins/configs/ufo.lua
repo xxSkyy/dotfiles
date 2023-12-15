7a417b7a0b59c6be33ee1e9cccaee479d09a8d48
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2023-12-16 00:17:53 +0100

    chore(neovim): disable ufo, causing huge issues
    hope temporarily

--- nvim/.config/nvim/lua/plugins/configs/ufo.lua
+++ nvim/.config/nvim/lua/plugins/configs/ufo.lua
@@ -1,9 +1,10 @@
 local language_servers = require("lspconfig").util.available_servers()
 
-for _, ls in ipairs(language_servers) do
-  require('lspconfig')[ls].setup({
-    capabilities = neovim.capabilities
-  })
-end
+-- for _, ls in ipairs(language_servers) do
+--   vim.notify(ls)
+--   require('lspconfig')[ls].setup({
+--     capabilities = neovim.capabilities
+--   })
+-- end
 
-require('ufo').setup()
+-- require('ufo').setup()
