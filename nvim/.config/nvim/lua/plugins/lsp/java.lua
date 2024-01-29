9a0d74e3dbc0b018222a0be6022187721d1f6456
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2024-01-29 15:59:22 +0100

    feat(neovim): update java config

--- nvim/.config/nvim/lua/plugins/lsp/java.lua
+++ nvim/.config/nvim/lua/plugins/lsp/java.lua
@@ -1,15 +1,23 @@
-local jdtls = require 'mason-core.path'.bin_prefix 'jdtls'
 local capabilities = neovim.capabilities
 
+local root_patterns = { 'gradlew', 'mvnw' }
+local matcher = require 'lspconfig'.util.root_pattern(root_patterns)
+
+local isJavaDir = matcher(vim.fn.getcwd())
+
+if (isJavaDir == nil) then
+  return
+end
+
+local jdtls = require 'mason-core.path'.bin_prefix 'jdtls'
 local config = {
   cmd = { jdtls },
-  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+  root_dir = vim.fs.dirname(vim.fs.find(root_patterns, { upward = true })[1]),
 }
 
 require('jdtls').start_or_attach(config)
 
-
 require 'lspconfig'.jdtls.setup {
   cmd = { jdtls },
-  capabilities = capabilities
+  capabilities = capabilities,
 }
