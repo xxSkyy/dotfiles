7fc8cd54611f91eac425b29f2d933f19eba2d281
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2024-01-29 15:59:11 +0100

    feat(neovim): update clangd config

--- nvim/.config/nvim/lua/plugins/lsp/c.lua
+++ nvim/.config/nvim/lua/plugins/lsp/c.lua
@@ -1,12 +1,22 @@
-require 'lspconfig'.sourcekit.setup {
-  capabilities = neovim.capabilities
-}
+-- require 'lspconfig'.sourcekit.setup {
+--   capabilities = neovim.capabilities
+-- }
 
+
+local clangd = "clangd"
+
+-- Mac path, modified for platformio and esp32 compatibility
+if vim.fn.has('macunix') == 1 then
+  clangd = vim.fn.expand('~/bin/clangd')
+end
 
 require 'lspconfig'.clangd.setup {
   capabilities = neovim.capabilities,
   cmd = {
     "clangd",
+    "--background-index",
     "--offset-encoding=utf-16",
+    -- "--query-driver=" .. vim.fn.expand("~/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-gcc*")
   },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 }
