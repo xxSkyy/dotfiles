e935e222ca86b1183e26392d391854d3b2ef3fe6
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2024-03-24 11:21:57 +0100

    perf: disable treesitter for illuminate provider
    huge performance issues on big files, after removing it - works flawless on even 10k lines files

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -230,7 +230,14 @@ require('lazy').setup({
     "RRethy/vim-illuminate",
     config = function()
       if not neovim.is_vscode() then
-        require('illuminate').configure({ modes_denylist = { 'v' } })
+        require('illuminate').configure({
+          modes_denylist = { 'v' },
+          providers = {
+            'lsp',
+            -- 'treesitter',
+            'regex',
+          },
+        })
       end
     end
   },
