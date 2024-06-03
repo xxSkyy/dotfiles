cba5dbb658f4bf179acd2fa1bcac41f06b64fbb9
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2024-06-03 04:27:24 +0200

    feat: finally vue works!
    new version works as typescript plugin and all updates works great

--- nvim/.config/nvim/lua/plugins/lsp/typescript.lua
+++ nvim/.config/nvim/lua/plugins/lsp/typescript.lua
@@ -1,56 +1,69 @@
-local have_vue = neovim.is_npm_package_installed 'vue'
+local mason_registry = require('mason-registry')
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() ..
+    '/node_modules/@vue/language-server'
 
+require 'lspconfig'.tsserver.setup {
+  root_dir = require 'lspconfig'.util.root_pattern("package.json"),
+  documentFeatures = {
+    documentFormatting = {
+      defaultPrintWidth = 60
+    },
+  },
+  server = {
+    on_attach = require("lsp-format").on_attach,
+    capabilities = neovim.capabilities
+  },
 
-if not have_vue then
-  require 'lspconfig'.tsserver.setup {
-    root_dir = require 'lspconfig'.util.root_pattern("package.json"),
-    server = {
-      on_attach = require("lsp-format").on_attach,
-      capabilities = neovim.capabilities
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" },
+  single_file_support = false,
+
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+      },
     },
-    -- neovim.is_npm_package_installed 'vue' and {} or
-    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-    single_file_support = false,
-    init_options = {
-      preferences = {
-        importModuleSpecifierPreference = "non-relative"
-      }
-    },
-    handlers = {
-      ["textDocument/publishDiagnostics"] = function(
+    preferences = {
+      importModuleSpecifierPreference = "non-relative"
+    }
+  },
+
+  handlers = {
+    ["textDocument/publishDiagnostics"] = function(
       _,
       result,
       ctx,
       config
+    )
+      if result.diagnostics == nil then
+        return
+      end
+
+      -- ignore some tsserver diagnostics
+      local idx = 1
+      while idx <= #result.diagnostics do
+        local entry = result.diagnostics[idx]
+
+        local formatter = require('format-ts-errors')[entry.code]
+        entry.message = formatter and formatter(entry.message) or entry.message
+
+        -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+        if entry.code == 80001 then
+          -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+          table.remove(result.diagnostics, idx)
+        else
+          idx = idx + 1
+        end
+      end
+
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        _,
+        result,
+        ctx,
+        config
       )
-        if result.diagnostics == nil then
-          return
-        end
-
-        -- ignore some tsserver diagnostics
-        local idx = 1
-        while idx <= #result.diagnostics do
-          local entry = result.diagnostics[idx]
-
-          local formatter = require('format-ts-errors')[entry.code]
-          entry.message = formatter and formatter(entry.message) or entry.message
-
-          -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-          if entry.code == 80001 then
-            -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-            table.remove(result.diagnostics, idx)
-          else
-            idx = idx + 1
-          end
-        end
-
-        vim.lsp.diagnostic.on_publish_diagnostics(
-          _,
-          result,
-          ctx,
-          config
-        )
-      end,
-    }
+    end,
   }
-end
+}
