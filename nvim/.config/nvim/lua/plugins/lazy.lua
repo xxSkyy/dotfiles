00b9e01116b94fd19381cba822f55d6ca03c69b8
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2024-06-26 17:30:59 +0200

    feat(nvim): dim indentation lines

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -312,13 +312,13 @@ require('lazy').setup({
       -- Create the highlight groups in the highlight setup hook, so they are reset
       -- every time the colorscheme changes
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "AutumnRed", { fg = "#C34043" })   -- AutumnRed
-        vim.api.nvim_set_hl(0, "OchreYellow", { fg = "#DCA561" }) -- OchreYellow
-        vim.api.nvim_set_hl(0, "WaveBlue", { fg = "#7FB4CA" })    -- WaveBlue
-        vim.api.nvim_set_hl(0, "PeachRed", { fg = "#FF9E3B" })    -- PeachRed
-        vim.api.nvim_set_hl(0, "SpringGreen", { fg = "#76946A" }) -- SpringGreen
-        vim.api.nvim_set_hl(0, "LightViolet", { fg = "#957FB8" }) -- LightViolet (custom)
-        vim.api.nvim_set_hl(0, "Teal", { fg = "#7AA89F" })        -- Teal
+        vim.api.nvim_set_hl(0, "AutumnRed", { fg = "#621E21" }) -- Dimmed AutumnRed
+        vim.api.nvim_set_hl(0, "OchreYellow", { fg = "#6E522F" }) -- Dimmed OchreYellow
+        vim.api.nvim_set_hl(0, "WaveBlue", { fg = "#3F5A65" }) -- Dimmed WaveBlue
+        vim.api.nvim_set_hl(0, "PeachRed", { fg = "#80501D" }) -- Dimmed PeachRed
+        vim.api.nvim_set_hl(0, "SpringGreen", { fg = "#3B4A35" }) -- Dimmed SpringGreen
+        vim.api.nvim_set_hl(0, "LightViolet", { fg = "#4B3D5C" }) -- Dimmed LightViolet
+        vim.api.nvim_set_hl(0, "Teal", { fg = "#3D5450" })  -- Dimmed Teal
       end)
 
       require("ibl").setup { indent = { highlight = highlight } }
