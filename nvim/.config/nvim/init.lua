84de04bdce54a180a2abb74a3f7ef35d1231d6c1
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2024-10-28 23:40:57 +0100

    style: formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,11 @@
---[[ init.lua ]]
-for _, source in ipairs { "utils" } do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then
-    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
-  end
+for _, source in ipairs({ "utils" }) do
+	local status_ok, fault = pcall(require, source)
+	if not status_ok then
+		vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+	end
 end
 
-require('config.vars')
-require('config.options')
-require('config.plugins')
-require('config.mappings')
+require("config.vars")
+require("config.options")
+require("config.plugins")
+require("config.mappings")
